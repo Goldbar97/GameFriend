44b9254f54cd06254d8fc8ff9111fc16b27b4b3a
Goldbar97 <ehrm1357@gmail.com>
2024-09-18 11:52:20 +0900

    REFACTOR: Added New Error Codes

--- src/main/java/com/gamefriend/exception/ErrorCode.java
+++ src/main/java/com/gamefriend/exception/ErrorCode.java
@@ -6,13 +6,14 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
+  ACCOUNT_LOCKED(423, "The account is temporary locked"),
   EMAIL_EXISTS(409, "Email is already exists"),
   EXPIRED_TOKEN(401, "Token is expired"),
   INTERNAL_SERVER_ERROR(500, "Internal server error"),
+  NOT_FOUND(404, "The resource doesn't exist"),
   UNAUTHORIZED(401, "Unauthorized access"),
   USER_NOT_FOUND(404, "User doesn't exist"),
   USING_SAME_PASSWORD(409, "The password is already in use"),
-  ACCOUNT_LOCKED(423, "The account is temporary locked"),
   WRONG_PASSWORD(400, "Wrong password");
 
   private final int statusCode;
