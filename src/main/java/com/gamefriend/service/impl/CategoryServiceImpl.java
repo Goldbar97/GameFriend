3e6071a2221c1b7da70a8ac1e3736bd77f4fc94e
Goldbar97 <ehrm1357@gmail.com>
2024-09-29 01:06:34 +0900

    feat: Add categoryDTO with rooms and participants info

--- src/main/java/com/gamefriend/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/gamefriend/service/impl/CategoryServiceImpl.java
@@ -22,11 +22,34 @@ public class CategoryServiceImpl implements CategoryService {
   @Transactional
   public void createCategory(CategoryDTO categoryDTO) {
 
+    if (categoryRepository.existsByName(categoryDTO.getName())) {
+      throw new CustomException(ErrorCode.CATEGORY_EXISTS);
+    }
+
     CategoryEntity categoryEntity = CategoryEntity.builder()
         .name(categoryDTO.getName())
+        .rooms(0)
+        .participants(0)
         .build();
 
     categoryRepository.save(categoryEntity);
+  }
+
+  @Override
+  @Transactional(readOnly = true)
+  public List<CategoryDTO> searchCategories(String query) {
+
+    List<CategoryEntity> categoryEntities = categoryRepository.findByQuery(query);
+
+    return categoryEntities.stream()
+        .map(e -> CategoryDTO.builder()
+            .id(e.getId())
+            .name(e.getName())
+            .rooms(e.getRooms())
+            .participants(e.getParticipants())
+            .build()
+        )
+        .collect(Collectors.toList());
   }
 
   @Override
@@ -40,6 +63,8 @@ public class CategoryServiceImpl implements CategoryService {
             CategoryDTO.builder()
                 .id(e.getId())
                 .name(e.getName())
+                .rooms(e.getRooms())
+                .participants(e.getParticipants())
                 .build()
         )
         .collect(Collectors.toList());
