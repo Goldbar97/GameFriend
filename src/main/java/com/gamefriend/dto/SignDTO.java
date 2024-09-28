c6c7f3405fd0cea47a7b9e93e94e7f16e8fdb442
Goldbar97 <ehrm1357@gmail.com>
2024-09-28 15:12:11 +0900

    feat: Change email to username

--- src/main/java/com/gamefriend/dto/SignDTO.java
+++ src/main/java/com/gamefriend/dto/SignDTO.java
@@ -2,11 +2,14 @@ package com.gamefriend.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Getter
+@NoArgsConstructor
 public class SignDTO {
 
-  private String email;
+  private String username;
+  private String nickname;
   private String password;
 }
