0c6e0b5079ef9592175bc6d3842b832ab4c618bd
Goldbar97 <ehrm1357@gmail.com>
2024-09-28 15:05:09 +0900

    feat: Add new error code

--- src/main/java/com/gamefriend/exception/ErrorCode.java
+++ src/main/java/com/gamefriend/exception/ErrorCode.java
@@ -9,11 +9,12 @@ public enum ErrorCode {
   ACCOUNT_LOCKED(423, "The account is temporary locked"),
   BAD_REQUEST(400, "Bad request"),
   CHATROOM_EXISTS(409, "Your chatroom is already exists"),
-  EMAIL_EXISTS(409, "Email is already exists"),
+  CHATROOM_FULL(400, "The chatroom is full"),
   EXPIRED_TOKEN(401, "Token is expired"),
   INTERNAL_SERVER_ERROR(500, "Internal server error"),
   NOT_FOUND(404, "The resource doesn't exist"),
   UNAUTHORIZED(401, "Unauthorized access"),
+  USERNAME_EXISTS(409, "Username is already exists"),
   USER_NOT_FOUND(404, "User doesn't exist"),
   USING_SAME_PASSWORD(409, "The password is already in use"),
   WRONG_PASSWORD(400, "Wrong password");
