08722d74a60ab560d52dda5140a9aa44f35eceef
Goldbar97 <ehrm1357@gmail.com>
2024-09-29 01:06:01 +0900

    feat: Cors set

--- src/main/java/com/gamefriend/security/SecurityConfig.java
+++ src/main/java/com/gamefriend/security/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.gamefriend.security;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,6 +12,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableMethodSecurity
@@ -24,7 +29,7 @@ public class SecurityConfig {
 
     http
         .csrf(AbstractHttpConfigurer::disable)
-        .cors(AbstractHttpConfigurer::disable)
+        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
         .httpBasic(AbstractHttpConfigurer::disable)
         .sessionManagement(e -> e.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(e -> e
@@ -32,10 +37,36 @@ public class SecurityConfig {
             .permitAll()
             .requestMatchers("/api/users/signin", "/api/users/signup", "/api/admins/signin")
             .permitAll()
-            .requestMatchers("/api/users/profile", "/api/users/password").authenticated()
+            .requestMatchers(HttpMethod.GET, "/api/categories", "/api/categories/search/**")
+            .permitAll()
+            .requestMatchers("/api/users/profile", "/api/users/password")
+            .authenticated()
             .anyRequest().authenticated())
         .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+
+    CorsConfiguration configuration = new CorsConfiguration();
+
+    // 모든 출처 허용
+    configuration.setAllowedOrigins(List.of("*"));
+
+    // 허용할 메서드 설정
+    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+
+    // 모든 헤더 허용
+    configuration.setAllowedHeaders(List.of("*"));
+
+    // 서버가 보내는 헤더 허용
+    configuration.setExposedHeaders(List.of("Authorization"));
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+
+    return source;
   }
 }
