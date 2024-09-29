c0e68355615b4c8bdb2cd8488b03d37da40555bb
Goldbar97 <ehrm1357@gmail.com>
2024-09-29 23:23:25 +0900

    feat: Add select all chatroomuser

--- src/main/java/com/gamefriend/repository/ChatRoomUserRepository.java
+++ src/main/java/com/gamefriend/repository/ChatRoomUserRepository.java
@@ -1,7 +1,9 @@
 package com.gamefriend.repository;
 
+import com.gamefriend.entity.ChatRoomEntity;
 import com.gamefriend.entity.ChatRoomUserEntity;
 import com.gamefriend.entity.UserEntity;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +13,6 @@ public interface ChatRoomUserRepository extends
     JpaRepository<ChatRoomUserEntity, Long> {
 
   Optional<ChatRoomUserEntity> findByUserEntity(UserEntity userEntity);
+
+  List<ChatRoomUserEntity> findAllByChatRoomEntity(ChatRoomEntity chatRoomEntity);
 }
