be6e2abb141d2775ba21f7baff3690b415020f8f
Goldbar97 <ehrm1357@gmail.com>
2024-09-29 23:22:46 +0900

    feat: Add FullText search query

--- src/main/java/com/gamefriend/repository/ChatRoomRepository.java
+++ src/main/java/com/gamefriend/repository/ChatRoomRepository.java
@@ -7,13 +7,22 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoomEntity, Long> {
 
   @Query("SELECT c FROM CHATROOM c JOIN FETCH c.userEntity WHERE c.categoryEntity = :categoryEntity")
-  List<ChatRoomEntity> findAllByCategoryEntity(CategoryEntity categoryEntity);
+  List<ChatRoomEntity> findAllByCategoryEntity(
+      @Param("categoryEntity") CategoryEntity categoryEntity);
 
   Optional<ChatRoomEntity> findByUserEntity(UserEntity userEntity);
+
+  @Query(value = "SELECT c.* FROM CHATROOM c "
+      + "WHERE MATCH(title) "
+      + "AGAINST(:query IN BOOLEAN MODE) "
+      + "AND c.category_entity_id = :categoryId", nativeQuery = true)
+  List<ChatRoomEntity> findByCategoryIdAndQuery(@Param("categoryId") Long categoryId,
+      @Param("query") String query);
 }
